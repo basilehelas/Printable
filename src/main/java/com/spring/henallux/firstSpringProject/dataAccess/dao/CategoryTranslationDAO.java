d96a6bf9d65f0416840bb132f1ebfbac76a31316
KraKss <97532245+KraKss@users.noreply.github.com>
2025-08-17 20:43:55 +0200

    fix category translation reset

--- src/main/java/com/spring/henallux/firstSpringProject/dataAccess/dao/CategoryTranslationDAO.java
+++ src/main/java/com/spring/henallux/firstSpringProject/dataAccess/dao/CategoryTranslationDAO.java
@@ -2,6 +2,7 @@ package com.spring.henallux.firstSpringProject.dataAccess.dao;
 
 import com.spring.henallux.firstSpringProject.dataAccess.repository.TranslationRepository;
 import com.spring.henallux.firstSpringProject.model.CategoryLabel;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -11,18 +12,15 @@ import java.util.stream.Collectors;
 @Service
 @Transactional(Transactional.TxType.SUPPORTS)
 public class CategoryTranslationDAO implements CategoryTranslationDataAccess {
-
     private final TranslationRepository translations;
 
     public CategoryTranslationDAO(TranslationRepository translations) {
         this.translations = translations;
     }
 
-
-
-
     @Override
-    public List<CategoryLabel> listCategoriesWithNames(String languageId) {
+    public List<CategoryLabel> listCategoriesWithNames() {
+        String languageId = LocaleContextHolder.getLocale().getLanguage(); // spring met localeCookie=en ou fr -> recherche de la locale
         return translations.findByIdLanguageId(languageId).stream()
                 .map(t -> new CategoryLabel(t.getId().getCategoryId(), t.getName()))
                 .collect(Collectors.toList());
